50224efebaf6758aeae34286641cea8778fceaa1
c.wong <coolpay64@gmail.com>
2021-10-28 00:48:47 +0800

    Rework on whole record reading function.

--- http-service.py
+++ http-service.py
@@ -1,9 +1,11 @@
 from typing import *
 from config import *
+from http_service_utils import *
+
+import gc
 import os
-import glob
 from collections import Counter
-from functools import lru_cache
+from time import perf_counter
 
 from fastapi import FastAPI
 # from fastapi.middleware.gzip import GZipMiddleware
@@ -11,30 +13,24 @@ from fastapi import FastAPI
 app = FastAPI()
 # app.add_middleware(GZipMiddleware)
 
-# Getting the name of the latest file as signature
-def get_latest_file() -> str:
-    return max(glob.iglob(DIR_DB_MERGED),key=os.path.getctime)
-
-def get_whole_file_list() -> List[str]:
-    # Filter .gitkeep
-    return [ f for f in os.listdir(DIR_DB_MERGED) if f[0] != '.' ]
-
 ## Routines to read all data records
-@lru_cache(2)
-def read_all_records_core(latest_file: str) -> List[Dict[str,str]]:
+@cache_on_latest_file
+def read_all_records() -> List[Dict[str,str]]:
     files = get_whole_file_list()
     return [
-        { 
+        {
             'time': ts,
-            'words' : open(os.path.join(DIR_DB_MERGED,ts)).readline()
+            'words': open(os.path.join(DIR_DB_MERGED, ts)).readline()
         }
         for ts in files
     ]
 
-def read_all_records() -> List[Dict[str,str]]:
-    # Cached core function + Signature
-    return read_all_records_core(get_latest_file())
-    
+@cache_on_latest_file
+def count_all_data() -> List[Dict[str,str]]:
+    recs = read_all_records()
+    all_texts = ''.join(rec['words'] for rec in recs)
+    return Counter(all_texts)
+
 @app.get("/")
 def read_root():
     return {'just to':'say hi, again.'}
@@ -51,8 +47,6 @@ def get_all_records():
 
 # API Call to count all data
 @app.get("/all/count")
-def count_all_records():
-    records = read_all_records()
-    return Counter(
-        ''.join([rec['words'] for rec in records])
-    )
+def get_all_count():
+    result =  count_all_data()
+    return result
