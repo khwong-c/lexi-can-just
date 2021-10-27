d92aae1acc022173e3c00c548727558e17198f60
KH Wong <c.wong@ximplar.com>
2021-10-27 17:56:21 +0800

    API call: return all data as time series, Count all data we have

--- http-service.py
+++ http-service.py
@@ -1,6 +1,7 @@
 from typing import *
 from config import *
 import os
+from collections import Counter
 
 from fastapi import FastAPI
 
@@ -14,3 +15,25 @@ def read_root():
 def update_service():
     os.system("git pull")
     return {'result':'done'}
+
+# API Call to get all data
+@app.get("/all")
+def read_all_records():
+    # Filter .gitkeep
+    files = [ f for f in os.listdir(DIR_DB_MERGED) if f[0] != '.' ]
+    return [
+        { 
+            'time': ts,
+            'words' : open(os.path.join(DIR_DB_MERGED,ts)).readline()
+        }
+        for ts in files
+    ]
+
+# API Call to count all data
+@app.get("/all/count")
+def count_all_records():
+    # Filter .gitkeep
+    files = [ f for f in os.listdir(DIR_DB_MERGED) if f[0] != '.' ]
+    return Counter(
+        ''.join([open(os.path.join(DIR_DB_MERGED,ts)).readline() for ts in files])
+    )
