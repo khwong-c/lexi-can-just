e82f2d197f4721c1f025564464340eb1f6e02cac
KH Wong <c.wong@ximplar.com>
2021-10-27 18:18:06 +0800

    Cache word records to avoid I/O access to the disk and speed up the service

--- http-service.py
+++ http-service.py
@@ -1,12 +1,39 @@
 from typing import *
 from config import *
 import os
+import glob
 from collections import Counter
+from functools import lru_cache
 
 from fastapi import FastAPI
 
+
 app = FastAPI()
 
+# Getting the name of the latest file as signature
+def get_latest_file() -> str:
+    return max(glob.iglob(DIR_DB_MERGED),key=os.path.getctime)
+
+def get_whole_file_list() -> List[str]:
+    # Filter .gitkeep
+    return [ f for f in os.listdir(DIR_DB_MERGED) if f[0] != '.' ]
+
+## Routines to read all data records
+@lru_cache(2)
+def read_all_records_core(latest_file: str) -> List[Dict[str,str]]:
+    files = get_whole_file_list()
+    return [
+        { 
+            'time': ts,
+            'words' : open(os.path.join(DIR_DB_MERGED,ts)).readline()
+        }
+        for ts in files
+    ]
+
+def read_all_records() -> List[Dict[str,str]]:
+    # Cached core function + Signature
+    return read_all_records_core(get_latest_file())
+    
 @app.get("/")
 def read_root():
     return {'just to':'say hi, again.'}
@@ -18,22 +45,13 @@ def update_service():
 
 # API Call to get all data
 @app.get("/all")
-def read_all_records():
-    # Filter .gitkeep
-    files = [ f for f in os.listdir(DIR_DB_MERGED) if f[0] != '.' ]
-    return [
-        { 
-            'time': ts,
-            'words' : open(os.path.join(DIR_DB_MERGED,ts)).readline()
-        }
-        for ts in files
-    ]
+def get_all_records():
+    return read_all_records()  
 
 # API Call to count all data
 @app.get("/all/count")
 def count_all_records():
-    # Filter .gitkeep
-    files = [ f for f in os.listdir(DIR_DB_MERGED) if f[0] != '.' ]
+    records = read_all_records()
     return Counter(
-        ''.join([open(os.path.join(DIR_DB_MERGED,ts)).readline() for ts in files])
+        ''.join([rec['words'] for rec in records])
     )
