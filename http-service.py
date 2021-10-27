72e7f467e3a0611d6d64f869932bffdc5f26301b
c.wong <coolpay64@gmail.com>
2021-10-28 01:22:19 +0800

    Web API for log access.

--- http-service.py
+++ http-service.py
@@ -5,7 +5,7 @@ from http_service_utils import *
 import gc
 import os
 from collections import Counter
-from time import perf_counter
+from functools import lru_cache
 
 from fastapi import FastAPI
 # from fastapi.middleware.gzip import GZipMiddleware
@@ -31,6 +31,9 @@ def count_all_data() -> List[Dict[str,str]]:
     all_texts = ''.join(rec['words'] for rec in recs)
     return Counter(all_texts)
 
+def read_log() -> List[str]:
+    return [line.strip() for line in open(FILE_LOG_CRAWL,'r').readlines()]
+
 @app.get("/")
 def read_root():
     return {'just to':'say hi, again.'}
@@ -50,3 +53,24 @@ def get_all_records():
 def get_all_count():
     result =  count_all_data()
     return result
+
+# API Call of Logs
+@app.get("/log/crawl")
+def get_log_crawl():
+    logs = [ line.split(' ') for line in read_log() if 'Crawl' in line ]
+    return [
+        {'time':' '.join((words[0],words[1])), 'words': int(words[-1])}
+        for words in logs
+    ]
+
+@app.get("/log/merge")
+def get_log_merge():
+    logs = [line.split(' ') for line in read_log() if 'Merge' in line]
+    return [
+        {'time': ' '.join((words[0], words[1])), 'files': int(words[-1])}
+        for words in logs
+    ]
+
+@app.get("/log/error")
+def get_log_crawl():
+    return [line for line in read_log() if 'ERROR' in line]
