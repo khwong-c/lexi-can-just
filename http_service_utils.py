3c6c54e19d7f590377ff758c68245a41235c2c3d
KH.Wong <kin.hin.wong.c@gmail.com>
2021-11-04 16:08:19 +0800

    Size control over caching decorator.

--- http_service_utils.py
+++ http_service_utils.py
@@ -3,6 +3,7 @@ import os
 import glob
 from config import *
 from functools import wraps
+from functools import lru_cache
 from time import perf_counter
 
 # Getting the name of the latest file as signature
@@ -22,18 +23,26 @@ def get_latest_file() -> str:
     files = [f for f in os.scandir(DIR_DB_MERGED)]
     return max(files,key=lambda f:f.stat().st_ctime).name
 
-def cache_on_latest_file(func):
-    @wraps(func)
-    def cached_function(*args,**kwargs):
-        sig = cached_function.signature
-        latest_name = get_latest_file()
-        if sig is None or sig != latest_name:
-            cached_function.signature = latest_name
-            cached_function.cached_result = func(*args,**kwargs)
-        return cached_function.cached_result
-    cached_function.signature = None
-    cached_function.cached_result = None
-    return cached_function
+def cache_on_latest_file(cache_size=1):
+    
+    def internal_deco(func):
+        @wraps(func)
+        def filename_cached_function(*args,**kwargs):
+            sig = filename_cached_function.signature
+            latest_name = get_latest_file()
+            if sig is None or sig != latest_name:
+                filename_cached_function.signature = latest_name
+                filename_cached_function.cached_function.cache_clear()
+            return filename_cached_function.cached_function(*args,**kwargs)
+        filename_cached_function.signature = None
+        filename_cached_function.cached_function = lru_cache(maxsize=cache_size)(func)
+        return filename_cached_function
+    
+    if callable(cache_size):
+        func, cache_size = cache_size, 1
+        return internal_deco(func)
+    else:
+        return internal_deco
 
 @cache_on_latest_file
 def get_whole_file_list() -> List[str]:
