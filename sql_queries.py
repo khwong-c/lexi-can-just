7987f950bcea937502b0a9500acf6d70f6382a45
KH.Wong <kin.hin.wong.c@gmail.com>
2021-11-04 19:10:47 +0800

    Add SQL queries for SQLite access

---
+++ sql_queries.py
@@ -0,0 +1,22 @@
+SQL_CREATE_TABLE = """CREATE TABLE IF NOT EXISTS lexican_history(
+    timestamp INTEGER UNIQUE
+                      NOT NULL
+                      PRIMARY KEY DESC,
+    interval  INTEGER NOT NULL,
+    chars     TEXT
+);
+"""
+SQL_INSERT_RECORD = """
+INSERT INTO lexican_history(timestamp, interval, chars) VALUES(?,?,?);
+"""
+SQL_SELECT_ALL_RECORD = """
+    SELECT timestamp, interval, chars FROM lexican_history;
+"""
+SQL_SELECT_RANGED_RECORD = """
+    SELECT timestamp, interval, chars 
+    FROM lexican_history
+    WHERE timestamp >= ? AND timestamp < ?;
+"""
+SQL_GET_LATEST_TIMESTAMP = """
+    SELECT timestamp FROM lexican_history ORDER BY timestamp DESC LIMIT 1;
+"""
